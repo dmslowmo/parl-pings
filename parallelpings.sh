bbb2ab55281b1eb605876126c7a017d22e132eb2
Martana Daniel <dmartana@hstcoredev02.amd.com>
2014-01-15 13:08:28 +0800

    source commit

---
+++ parallelpings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+argc=$#
+if [ $# -lt 1 ]
+then
+   echo "Usage: $0 <ip-list-file>"
+   exit 1
+fi
+
+function customping 
+{
+  ping -c 1 -W 1 $1 >/dev/null 2>&1 && echo "$1 is up" || echo "$1 is down"
+#  sleep 0.01s
+}
+
+T="$(date +%s%N)"
+
+defaultno=24
+noofproc=$defaultno
+
+if [ -n "$2" ] #user-set no. of process instead of default
+then
+noofproc=$2
+echo "Max processes: $noofproc"
+fi
+
+export -f customping && cat $1 | xargs -n 1 -P $noofproc -I{} bash -c 'customping "$@"' _ {} \;
+
+T="$(($(date +%s%N)-T))"
+MS="$((T/1000000))"
+echo "Elapsed time: ${MS} ms"
